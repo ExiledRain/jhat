fddd761cfa7056524a161ea2b13f603b42fedd0d
ExiledRain <gett_lostt@bk.ru>
2021-02-18 20:28:48 +0200

    Initial commit

---
+++ src/main/java/io/exiled/jhat/JhatApplication.java
@@ -0,0 +1,13 @@
+package io.exiled.jhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhatApplication.class, args);
+	}
+
+}
